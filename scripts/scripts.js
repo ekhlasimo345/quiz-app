576a3f2b619ea65a2732204c850cf81ab39402af
mohammad <ekhlasimohammad345@gmail.com>
2025-04-11 17:36:47 +0200

    Changeing names

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,228 +2,241 @@ let questions=
 [
    {  
       questionNumber1: "What is the force that pulls objects toward the Earth?" , 
-         AmswerA:" Magnetism " ,
-         AmswerB:" Gravity ",
-         AmswerC:" Electricity",  
-         AmswerD:" Wind  ",
+         text:" Magnetism " ,
+         text:" Gravity ",
+         text:" Electricity",  
+         text:" Wind  ",
 
          correctAnswer:"Gravity"
    },
    {
       questionNumber2: "What do we call frozen water?"  ,
-         AmswerA:" Ice"  ,
-         AmswerB:" Steam" , 
-         AmswerC:" Rain " ,
-         AmswerD:" Fog ",
+         text:" Ice"  ,
+         text:" Steam" , 
+         text:" Rain " ,
+         text:" Fog ",
 
          correctAnswer:"Ice"
    },
    {
       questionNumber3: "What happens when you mix red and blue light?"  ,
-         AmswerA:" Green  ",
-         AmswerB:" Yellow " ,
-         AmswerC:" Purple " ,
-         AmswerD:" Orange  ",
+         text:" Green  ",
+         text:" Yellow " ,
+         text:" Purple " ,
+         text:" Orange  ",
    
          correctAnswer:"Purple"
    },
    {
       questionNumber4: "Which one of these is a source of light?" ,
-         AmswerA:" The Moon  ",
-         AmswerB:" A Mirror  ",
-         AmswerC:" A Flashlight ",  
-         AmswerD:" A Rock " ,
+         text:" The Moon  ",
+         text:" A Mirror  ",
+         text:" A Flashlight ",  
+         text:" A Rock " ,
 
          correctAnswer:"A Flashlight"
    },
    {     
       questionNumber5: "What do we use to measure temperature?" , 
-         AmswerA:" Ruler  ",
-         AmswerB:" Scale  ",
-         AmswerC:" Thermometer  ", 
-         AmswerD:" Clock  ",
+         text:" Ruler  ",
+         text:" Scale  ",
+         text:" Thermometer  ", 
+         text:" Clock  ",
 
          correctAnswer:"Thermometer"
    },
    {      
       questionNumber6: "What is the chemical formula for water?" , 
-         AmswerA:" O₂  ",
-         AmswerB:" H₂O   ",
-         AmswerC:" CO₂  ",
-         AmswerD:" NaCl  ",
+         text:" O₂  ",
+         text:" H₂O   ",
+         text:" CO₂  ",
+         text:" NaCl  ",
 
          correctAnswer:"H₂O"
    },
    {      
       questionNumber7: "What is the color of most leaves?",  
-         AmswerA:" Yellow  ",
-         AmswerB:" Blue  ",
-         AmswerC:" Green  ", 
-         AmswerD:" Red  ",
+         text:" Yellow  ",
+         text:" Blue  ",
+         text:" Green  ", 
+         text:" Red  ",
 
          correctAnswer:"Green"
    },
    {     
       questionNumber8: "Which of these is a gas?",  
-         AmswerA:" Ice  ",
-         AmswerB:" Water  ",
-         AmswerC:" Oxygen " , 
-         AmswerD:" Sand  ",
+         text:" Ice  ",
+         text:" Water  ",
+         text:" Oxygen " , 
+         text:" Sand  ",
 
          correctAnswer:"Oxygen"
    },
    {     
       questionNumber9: "What happens when you mix blue and yellow paint?",  
-         AmswerA:" Red  ",
-         AmswerB:" Green  ", 
-         AmswerC:" Purple " ,
-         AmswerD:" Orange  ",
+         text:" Red  ",
+         text:" Green  ", 
+         text:" Purple " ,
+         text:" Orange  ",
 
          correctAnswer:"Green"
    },
    {     
       questionNumber10: "What do we call a change where a new substance is formed?",
-         AmswerA:" Physical change  ",
-         AmswerB:" Chemical change   ",
-         AmswerC:" Color change  ",
-         AmswerD:" Size change  ",
+         text:" Physical change  ",
+         text:" Chemical change   ",
+         text:" Color change  ",
+         text:" Size change  ",
 
          correctAnswer:"Chemical change"
    },
    {     
       questionNumber11: "What is the name of the outer part of a tree?" ,
-         AmswerA:" Skin  ",
-         AmswerB:" Shell  ",
-         AmswerC:" Bark   ",
-         AmswerD:" Trunk  ",
+         text:" Skin  ",
+         text:" Shell  ",
+         text:" Bark   ",
+         text:" Trunk  ",
 
          correctAnswer:"Bark"
    },
    {     
       questionNumber12: "What is the name of the organ that pumps blood in our body?" , 
-         AmswerA:" Brain  ",
-         AmswerB:" Stomach  ",
-         AmswerC:" Heart   ",
-         AmswerD:" Lungs  ",
+         text:" Brain  ",
+         text:" Stomach  ",
+         text:" Heart   ",
+         text:" Lungs  ",
 
          correctAnswer:"Heart"
    },
    {     
       questionNumber13: "Which animal is the largest on Earth?",
-         AmswerA:" Elephant  ",
-         AmswerB:" Blue Whale ",
-         AmswerC:" Giraffe ",
-         AmswerD:" Shark ",
+         text:" Elephant  ",
+         text:" Blue Whale ",
+         text:" Giraffe ",
+         text:" Shark ",
 
          correctAnswer:"Blue Whale"
    },
    {     
       questionNumber14: "What do bees collect from flowers?" , 
-         AmswerA:" Water  ",
-         AmswerB:" Pollen  ",
-         AmswerC:" Leaves " ,
-         AmswerD:" Seeds  ",
+         text:" Water  ",
+         text:" Pollen  ",
+         text:" Leaves " ,
+         text:" Seeds  ",
 
          correctAnswer:"Pollen"
    },
    {     
       questionNumber15: "Which of these animals lays eggs?", 
-         AmswerA:" Dog  ",
-         AmswerB:" Cat  ",
-         AmswerC:" Snake  ",
-         AmswerD:" Horse ",
+         text:" Dog  ",
+         text:" Cat  ",
+         text:" Snake  ",
+         text:" Horse ",
 
          correctAnswer:"Snake"
    },
    {     
       questionNumber16: "What is the name of the star at the center of our Solar System?", 
-         AmswerA:" The Moon  ",
-         AmswerB:" The Sun   ",
-         AmswerC:" The Earth  ",
-         AmswerD:" Mars  ",
+         text:" The Moon  ",
+         text:" The Sun   ",
+         text:" The Earth  ",
+         text:" Mars  ",
 
          correctAnswer:"The Sun"
    },
    {     
       questionNumber17: "What do we call a scientist who studies space?",
-         AmswerA:" Geologist  ",
-         AmswerB:" Astronomer  ",
-         AmswerC:" Biologist  ",
-         AmswerD:" Chemist  ",
+         text:" Geologist  ",
+         text:" Astronomer  ",
+         text:" Biologist  ",
+         text:" Chemist  ",
 
          correctAnswer:"Astronomer"
    },
    {     
       questionNumber18: "What are rocks that fall from space called?" , 
-         AmswerA:" Asteroids " ,
-         AmswerB:" Comets  ",
-         AmswerC:" Meteors  ", 
-         AmswerD:" Planets  ",
+         text:" Asteroids " ,
+         text:" Comets  ",
+         text:" Meteors  ", 
+         text:" Planets  ",
 
          correctAnswer:"Meteors"
    },
    {     
       questionNumber19:"Which season comes after summer?",
-         AmswerA:" Winter  ",
-         AmswerB:" Spring  ",
-         AmswerC:" Fall (Autumn)",   
-         AmswerD:" Summer  ",
+         text:" Winter  ",
+         text:" Spring  ",
+         text:" Fall (Autumn)",   
+         text:" Summer  ",
 
          correctAnswer:"Fall (Autumn)"
    },
    {     
       questionNumber20: "What do we call the shape of the Earth?" , 
-         AmswerA:" Flat  ",
-         AmswerB:" Cube  ",
-         AmswerC:" Sphere ",  
-         AmswerD:" Pyramid ", 
+         text:" Flat  ",
+         text:" Cube  ",
+         text:" Sphere ",  
+         text:" Pyramid ", 
 
          correctAnswer:"Sphere"
    },
    {     
       questionNumber21: "What is the name of the gas we breathe in to stay alive?",
-         AmswerA:" Carbon dioxide"  ,
-         AmswerB:" Oxygen  ",
-         AmswerC:" Helium  ",
-         AmswerD:" Hydrogen ", 
+         text:" Carbon dioxide"  ,
+         text:" Oxygen  ",
+         text:" Helium  ",
+         text:" Hydrogen ", 
 
          correctAnswer:"Oxygen"
    },
    {     
       questionNumber22: "What do you call a baby dog?"  ,
-         AmswerA:" Kitten " ,
-         AmswerB:" Calf " ,
-         AmswerC:" Pup (Puppy)",   
-         AmswerD:" Cub " ,
+         text:" Kitten " ,
+         text:" Calf " ,
+         text:" Pup (Puppy)",   
+         text:" Cub " ,
 
          correctAnswer:"Pup (Puppy)"
    },
    {     
       questionNumber23: "What do plants need to make their own food?",  
-         AmswerA:" Sunlight "  ,
-         AmswerB:" Rocks  ",
-         AmswerC:" Sand  ",
-         AmswerD:" Wind  ",
+         text:" Sunlight "  ,
+         text:" Rocks  ",
+         text:" Sand  ",
+         text:" Wind  ",
 
          correctAnswer:"Sunlight"
    },
    {     
       questionNumber24: "What is the name of the largest ocean on Earth?",
-         AmswerA:" Atlantic Ocean"  ,
-         AmswerB:" Indian Ocean"  ,
-         AmswerC:" Pacific Ocean"  , 
-         AmswerD:" Arctic Ocean"  ,
+         text:" Atlantic Ocean"  ,
+         text:" Indian Ocean"  ,
+         text:" Pacific Ocean"  , 
+         text:" Arctic Ocean"  ,
 
          correctAnswer:"Pacific Ocean"
    },
    {     
       questionNumber25: "What sense do we use our ears for?",
-         AmswerA:" Sight " ,
-         AmswerB:" Smell  ",
-         AmswerC:" Hearing" ,  
-         AmswerD:" Taste"  ,
+         text:" Sight " ,
+         text:" Smell  ",
+         text:" Hearing" ,  
+         text:" Taste"  ,
 
          correctAnswer:"Hearing" ,
    }
 ]     
 
+const printQuestions = questions.map(() => {
+   return `
+   <div class="question">
+      <h2>${questions.questionNumber1}</h2
+      <ul
+         <li>${questions.text}</li>
+         <li>${questions.text}</li>
+         <li>${questions.text}</li>
+         <li>${questions.text}</li>
+      </ul>
+   </div>
+   `;
+})
